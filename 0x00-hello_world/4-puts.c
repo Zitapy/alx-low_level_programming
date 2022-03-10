4733c66de1a57818b5b70d031da678d5846cf68a
root <estherzita1993@gmail.com>
2022-03-10 11:28:09 -0800

    Put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <studio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(viod)
+{
+   char test_str[] = "\"Programming is like building a multilingual puzzle";
+
+   puts(test_str);
+    return (0);
+}   
