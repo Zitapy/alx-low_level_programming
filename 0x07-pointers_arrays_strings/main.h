6f8ef250a68dbd58aa6698c849e1bb8f8516322d
root <estherzita1993@gmail.com>
2022-03-30 07:50:41 -0700

    main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,10 +1,23 @@
-int _putchar(char c);
-char *_memset(char *s, char b, unsigned int n);
-char *_memset(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle);
-void print_chessboard(char (*a)[8]);
-void print_diagsums(int *a, int size);
-void set_string(char **s, char *to);
+#include "main.h"
+/**
+ *_memset -   fills memory with a constant byte
+ *@s: pointer block of memory to fill
+ *@b: value to set
+ *@n: bytes of the memory
+ *Return: dest
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+
+{
+	unsigned int i;
+
+	/*Delacring FOR*/
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b; /*add 1 position s*/
+
+	} /*END FOR*/
+
+	return (s);
+}
